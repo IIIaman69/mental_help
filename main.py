f60230618dd88c2b6a4601c7bb543c60ff399510
IIIaman69 <burdovdavid@yandex.ru>
2023-06-02 16:42:15 +0500

    add answer to question

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ bot = telebot.TeleBot('5885143732:AAFoAMITlbkmlQzUEFc0BXMWiAmTDq5av3Y')
 def main(message):
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
 
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+
 bot.polling(none_stop=True)
