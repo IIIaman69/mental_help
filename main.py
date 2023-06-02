4cedf9b2057533b04468cbcb24d144997b80b959
IIIaman69 <burdovdavid@yandex.ru>
2023-06-02 16:33:11 +0500

    add hello

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import telebot
 
 bot = telebot.TeleBot('5885143732:AAFoAMITlbkmlQzUEFc0BXMWiAmTDq5av3Y')
 
-
 @bot.message_handler(commands=['start', 'hello'])
 def main(message):
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
