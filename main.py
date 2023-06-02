319a6801615182fbf40f60d4ea3f783c942c84b8
IIIaman69 <burdovdavid@yandex.ru>
2023-06-02 16:31:58 +0500

    add hello

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Help")
+import telebot
+
+bot = telebot.TeleBot('5885143732:AAFoAMITlbkmlQzUEFc0BXMWiAmTDq5av3Y')
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def main(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+
+bot.polling(none_stop=True)
