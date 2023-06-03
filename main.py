6133ab29b75e04e20bdcfc82cb2c66c709c31471
IIIaman69 <burdovdavid@yandex.ru>
2023-06-03 12:48:41 +0500

    integrate download link book in button

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot('5885143732:AAFoAMITlbkmlQzUEFc0BXMWiAmTDq5av3Y')
 #TO_CHAT_ID = -1001980809298
 with open('aforism.txt','r', encoding="utf-8") as aforism:
     aforism = aforism.readlines()
+with open('book.txt', 'r', encoding='utf-8') as book:
+    book = book.read()
 
 @bot.message_handler(commands=['start', 'hello'])
 def start(message):
@@ -24,7 +26,17 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def callback_message(message):
     if message.text == 'Книги':
-        bot.send_message(message.chat.id, 'Website is open')
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton('1', url='https://disk.yandex.ru/i/jboYx_IT2zGaVQ')
+        btn2 = types.InlineKeyboardButton('2', url='https://disk.yandex.ru/i/hyhiIOY1OifQpg')
+        markup.row(btn1, btn2)
+        btn3 = types.InlineKeyboardButton('3', url='https://disk.yandex.ru/i/RuG13CPQItkeZg')
+        btn4 = types.InlineKeyboardButton('4', url='https://disk.yandex.ru/i/kq7LtWYfgOLtBQ')
+        markup.row(btn3, btn4)
+        btn5 = types.InlineKeyboardButton('5', url='https://disk.yandex.ru/i/7n8Iqlm8U2snbA')
+        btn6 = types.InlineKeyboardButton('6', url='https://disk.yandex.ru/i/Mk0uyj0OOD6RiQ')
+        markup.row(btn5, btn6)
+        bot.send_message(message.chat.id, book, reply_markup=markup)
     elif message.text == 'Почта для предложений':
         bot.send_message(message.chat.id, 'Направляйте по этому адресу ваши пожелания, а также книги и статьи, '
                                           'которые морально или эмоционально помогли вам: 6moonwolf6@gmail.com')
@@ -48,10 +60,10 @@ def get_photo(message):
 
 '''@bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
-    if callback.data == 'delete':
-        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
-    elif callback.data == 'edit':
-        bot.edit_message_text('Edit', callback.message.chat.id, callback.message.message_id)'''
+    if callback.data == '1':
+        bot.sent_message(callback.message.chat.id, url='https://disk.yandex.ru/i/jboYx_IT2zGaVQ')
+    elif callback.data == '2':
+        bot.sent_message(callback.message.chat.id, url='https://disk.yandex.ru/i/hyhiIOY1OifQpg')'''
 
 '''@bot.message_handler()
 def info(message):
